5b387b2f48b9be0104546f795b7757508ffac383
vincent <vincentmald@gmail.com>
2019-11-04 11:51:28 +0100

    initial commit

--- Todo/TodoListViewController.swift
+++ Todo/TodoListViewController.swift
@@ -57,6 +57,7 @@ class TodoListViewController: UITableViewController {
             self.itemArray.append(newItem)
             //self.defaults.set(<#T##url: URL?##URL?#>, forKey: <#T##String#>)
             self.tableView.reloadData()
+            test.
         }
         
         alert.addTextField { (alertTextField) in
